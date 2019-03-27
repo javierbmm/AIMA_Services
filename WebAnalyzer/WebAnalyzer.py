bd203f9d7ea7c657e59a0b944166c8b5190b584d
javierbmm <javiermerida05@gmail.com>
2019-03-27 01:00:33 +0100

    Little change. Nothing important.

--- WebAnalyzer/WebAnalyzer.py
+++ WebAnalyzer/WebAnalyzer.py
@@ -112,7 +112,7 @@ def bot_send_msg(msg):
     bot_token = '656778310:AAHyZaNhAQwVYitZcIHAfi2TmQN_CBKdOIU'
     #Insert your ID below. 
     #AIMA_ID = '700187299' <- for AIMA_Services 
-    bot_chatID = JAVIER_ID  
+    bot_chatID = AIMA_ID  
     send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + msg
     response = requests.get(send_text)
 
