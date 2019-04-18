9b2828c42e6183f9882d5469bafe8a06e6434c26
javierbmm <javiermerida05@gmail.com>
2019-04-18 03:01:22 +0200

    Update Installer.sh

--- WebAnalyzer/Linux/Installer.sh
+++ WebAnalyzer/Linux/Installer.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 sudo chmod u+x Installer.sh
 
+echo "Installing Chrome"
+sudo curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add
+sudo echo "deb [arch=amd64]  http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
+sudo apt-get -y update
+sudo apt-get -y install google-chrome-stable
+
 echo "Setting virtual environment"
 virtualenv --python python3 .
 nohup sleep 10 &
