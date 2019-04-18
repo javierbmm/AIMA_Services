9d784514d5b26f78e88dd57a4bab1624fc68c577
javierbmm <javiermerida05@gmail.com>
2019-04-18 22:32:45 +0200

    Update Installer.sh

--- WebAnalyzer/Linux/Installer.sh
+++ WebAnalyzer/Linux/Installer.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 sudo chmod u+x Installer.sh
 
+echo "Installing Python, pip and virtualenv"
+sudo apt update
+sudo apt install python python-dev python3 python3-dev
+wget https://bootstrap.pypa.io/get-pip.py
+sudo python get-pip.py
+pip install --upgrade virtualenv
+
 echo "Installing Chrome"
 sudo curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add
 sudo echo "deb [arch=amd64]  http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
