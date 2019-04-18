f48b1ece6bf17efefa715f9dd952e33f970c4960
javierbmm <javiermerida05@gmail.com>
2019-04-18 02:46:36 +0200

    Update Installer.sh

--- WebAnalyzer/Linux/Installer.sh
+++ WebAnalyzer/Linux/Installer.sh
@@ -4,10 +4,8 @@ sudo chmod u+x Installer.sh
 echo "Setting virtual environment"
 cd .. 
 virtualenv --python python3 .
-nohup sleep 10 &
-source bin/activate
+source ../bin/activate
 cd WebAnalyzer/Linux
 echo "Installing requirements"
 pip install -r requirements.txt
-nohup sleep 10 &
 echo "Installation successfully done"
