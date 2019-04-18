5ff668a78c858080f4234209c01bd2fff09d7236
javierbmm <javiermerida05@gmail.com>
2019-04-19 01:44:24 +0200

    Update WebAnalyzer.py

--- WebAnalyzer/Linux/WebAnalyzer.py
+++ WebAnalyzer/Linux/WebAnalyzer.py
@@ -248,7 +248,7 @@ def click_español(browser):
 
 def click_futbol_section(browser):
    # WebDriverWait(browser,100).until(EC.presence_of_element_located((By.XPATH, XPATH_ESPAÑOL))) 
-    browser.switch_to_window(browser.window_handles[1])
+    #browser.switch_to_window(browser.window_handles[1])
 
     futbol_section = browser.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[1]/div/div[1]/div/div/div[16]')
     sleep(delay[randint(0,4)]) # Time in seconds.
