12cab7c54a3e70ff2ccf0f12cb6e58250674c40d
javierbmm <javiermerida05@gmail.com>
2019-04-19 02:55:08 +0200

    Update WebAnalyzer.py

--- WebAnalyzer/Linux/WebAnalyzer.py
+++ WebAnalyzer/Linux/WebAnalyzer.py
@@ -243,7 +243,7 @@ def click_español(browser):
     #Navigation:
     sleep(delay[randint(0,4)]) # Time in seconds.
     sleep(delay[randint(0,4)]) # Time in seconds.
-    español = browser.find_element_by_link_text("Español").click()
+    español = browser.find_element_by_link_text("Español")
     español.click()
     sleep(delay[randint(0,4)]) # Time in seconds.
   
