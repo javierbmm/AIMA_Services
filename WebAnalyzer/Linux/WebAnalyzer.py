f86f1b7e5a5f950eec0c5bd7318092206bc41901
javierbmm <javiermerida05@gmail.com>
2019-04-19 03:23:19 +0200

    Update WebAnalyzer.py

--- WebAnalyzer/Linux/WebAnalyzer.py
+++ WebAnalyzer/Linux/WebAnalyzer.py
@@ -226,12 +226,10 @@ def open_website(url):
     options.headless    = True
     user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36'
     options.add_argument('user-agent='+user_agent)
-    options.add_argument('--start-maximized')
     options.add_argument('--no-sandbox')
-    options.add_argument('--ignore-certificate-errors')
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--window-size=1920,1080")
-    options.add_argument("--disable-gpu")
+    #options.add_argument("--disable-gpu")
 
     #browser = webdriver.Chrome(ChromeDriverManager().install())
     # TODO: Catch exception when unable to open website
