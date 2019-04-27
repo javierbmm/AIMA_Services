5bcd46692ddd47136a7dee2b901658b3c1c48f20
javierbmm <javiermerida05@gmail.com>
2019-04-27 04:10:26 +0200

    Update WebAnalyzer.py

--- WebAnalyzer/Linux-testing/WebAnalyzer.py
+++ WebAnalyzer/Linux-testing/WebAnalyzer.py
@@ -895,7 +895,7 @@ def main():
     dict_updated = True
     file_name = "./matchesFile.txt"
     tomorrow = date.today() + timedelta(days=1)
-    tomorrow_0h = datetime.now() #datetime(tomorrow.year, tomorrow.month, tomorrow.day, 0, 0, 0)
+    tomorrow_0h = datetime(tomorrow.year, tomorrow.month, tomorrow.day, 0, 0, 0)
 
     while True:
         sleep(30) # 30 secs
