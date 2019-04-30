a05269d16cae75b4776f214faeab401f865d7261
javierbmm <javiermerida05@gmail.com>
2019-04-30 21:50:10 +0200

    Update WebAnalyzer.py

--- WebAnalyzer/Linux/WebAnalyzer.py
+++ WebAnalyzer/Linux/WebAnalyzer.py
@@ -236,7 +236,7 @@ def send_msg_by_groups(bot_message):
     print('Sending information')
     bot_send_msg(msg)
 
-    #send_msg_by_groups_to(bot_message, OTHER_ID)
+    send_msg_by_groups_to(bot_message, OTHER_ID)
 
     return
 
@@ -265,7 +265,7 @@ def bot_send_msg(msg):
     bot_token = '656778310:AAHyZaNhAQwVYitZcIHAfi2TmQN_CBKdOIU'
     # Insert your ID below.
     # AIMA_ID = '700187299' <- for AIMA_Services
-    bot_chatID = JAVIER_ID
+    bot_chatID = AIMA_ID
     send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + msg
     response = requests.get(send_text)
 
