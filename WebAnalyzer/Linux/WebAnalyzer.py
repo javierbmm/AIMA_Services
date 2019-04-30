207ea65a60773e5712a5bbd5b2c1f26e0c415cae
javierbmm <javiermerida05@gmail.com>
2019-04-30 21:50:46 +0200

    Update WebAnalyzer.py

--- WebAnalyzer/Linux/WebAnalyzer.py
+++ WebAnalyzer/Linux/WebAnalyzer.py
@@ -687,7 +687,8 @@ def extract_live_matches_information(browser, match_dict):
         option = 'OVER ' + str(result[1])
         ft_checked = True
         from_dict.set_ft_checked(ft_checked)
-    else: return live_match_info_
+    else:
+        return live_match_info_
 
     if float(fee) < 0: return live_match_info_
     print('got it')
