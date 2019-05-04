89d2f7038dfcd7ffa49f22e378167c3a3bf10efc
javierbmm <javiermerida05@gmail.com>
2019-05-04 02:15:21 +0200

    Update WebAnalyzer.py

--- WebAnalyzer/Linux/WebAnalyzer.py
+++ WebAnalyzer/Linux/WebAnalyzer.py
@@ -20,7 +20,7 @@ from time import sleep
 from random import randint
 import traceback
 
-delay = [1, 2, 1.5, 2, 2.3]
+delay = [1, 1.4, 1.3, 1.2, 1.1]
 LINE = "\n--------------------------------------------\n"
 # Users ID's:
 AIMA_ID = '846646570'  # AIMA_Services
@@ -236,7 +236,7 @@ def send_msg_by_groups(bot_message):
     print('Sending information')
     bot_send_msg(msg)
 
-    send_msg_by_groups_to(bot_message, OTHER_ID)
+    #send_msg_by_groups_to(bot_message, OTHER_ID)
 
     return
 
@@ -265,7 +265,7 @@ def bot_send_msg(msg):
     bot_token = '656778310:AAHyZaNhAQwVYitZcIHAfi2TmQN_CBKdOIU'
     # Insert your ID below.
     # AIMA_ID = '700187299' <- for AIMA_Services
-    bot_chatID = AIMA_ID
+    bot_chatID = JAVIER_ID
     send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + msg
     response = requests.get(send_text)
 
@@ -802,7 +802,7 @@ def get_live_matches(browser, msg, league, match_dict):
             print(match.text)
         except Exception:
             print(traceback.print_exc())
-            browser.save_screenshot("error.png")
+            #browser.save_screenshot("error.png")
             counter += 1
             continue
         try:
@@ -1017,7 +1017,7 @@ def main():
 
         #end try-except
         print("ending while")
-        sleep(5*60)
+        #sleep(5*60)
 
     #end while
 
